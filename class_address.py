7311eeca53fdefcb80729c26e498d953a1c7a2e6
Gareth Gwyther <gareth.gwyther@gmail.com>
2017-05-12 21:51:18 +0100

    Added Error Function checking

--- class_address.py
+++ class_address.py
@@ -5,5 +5,8 @@ class Address:
     address_line_4 = ""
     postcode = ""
 
+    def checkErrors(self):
+        return ""
+
     def display(self):
         return self.address_line_1+" "+self.address_line_2+" "+self.address_line_3+" "+self.address_line_4
